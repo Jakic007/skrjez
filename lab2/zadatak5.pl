10ac7babfbbf5bb6cc0259eb38d7c9b45130b69e
Jakov Buratovic <kraljipo007@gmail.com>
2020-04-15 18:35:36 +0200

    task 5

--- lab2/zadatak5.pl
+++ lab2/zadatak5.pl
@@ -7,7 +7,21 @@ while (<>) {
     if($_ =~ m/[0-9].[0-9]{2};[0-9].[0-9]{2};[0-9].[0-9]{2};[0-9].[0-9]{2};[0-9].[0-9]{2};[0-9].[0-9]{2};[0-9].[0-9]{2}/){
         @points = split /;/;
     } elsif ($_ =~ m/[0-9]{10};/){
-        print "$_\n";
+        @student = split /;/;
+        $marks = 0;
+
+        for($i=0;$i<@points;$i++) {
+			if($student[$i+3] ne '-') {
+				$marks += $student[$i+3] * $points[$i];
+			}
+		}
+        $position{$student[0]} = sprintf("%.2f",$marks);
+		$identity{$student[0]} = $student[1].", ".$student[2];
     }
-	
+}
+$i = 0;
+print "Lista po rangu:\n-------------------\n";
+foreach $key (sort { $position{$b} <=> $position{$a} } keys %position) {
+    $i++;
+	print "$i. $identity{$key} ($key) : $position{$key}\n";
 }
